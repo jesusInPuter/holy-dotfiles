16111c1606dfe778ad17e36afc8afbd26304a7cb
jesus <theaminishere@gmail.com>
2025-09-25 02:34:25 +0530

    yea

--- nvim/lua/plugins/user.lua
+++ nvim/lua/plugins/user.lua
@@ -10,30 +10,32 @@ return {
   {
     "windwp/nvim-autopairs",
     config = function(plugin, opts)
-      require "astronvim.plugins.configs.nvim-autopairs" (plugin, opts) -- include the default astronvim config that calls the setup call
-      -- add more custom autopairs configuration such as custom rules
+      require "astronvim.plugins.configs.nvim-autopairs" (plugin, opts) -- default config
+
       local npairs = require "nvim-autopairs"
       local Rule = require "nvim-autopairs.rule"
       local cond = require "nvim-autopairs.conds"
+
       npairs.add_rules(
         {
           Rule("$", "$", { "tex", "latex" })
-          -- don't add a pair if the next character is %
               :with_pair(cond.not_after_regex "%%")
-          -- don't add a pair if  the previous character is xxx
-              :with_pair(
-                cond.not_before_regex("xxx", 3)
-              )
-          -- don't move right when repeat character
+              :with_pair(cond.not_before_regex("xxx", 3))
               :with_move(cond.none())
-          -- don't delete if the next character is xx
               :with_del(cond.not_after_regex "xx")
-          -- disable adding a newline when you press <cr>
               :with_cr(cond.none()),
         },
-        -- disable for .vim files, but it work for another filetypes
         Rule("a", "a", "-vim")
       )
+
+      -- Minimal highlight for the active pair
+      vim.cmd [[
+      hi! link NPairsMatch CursorLineNr
+    ]]
+      npairs.setup {
+        highlight = "NPairsMatch",  -- highlight active pair
+        highlight_grey = "Comment", -- non-active pairs dimmed
+      }
     end,
   },
 }
