243ee1ad940ae7d1f100a707c7d066a31aabd3ce
jesus <theaminishere@gmail.com>
2025-09-26 20:12:39 +0530

    tweaking emacs

--- doom/config.el
+++ doom/config.el
@@ -1,54 +1,30 @@
 ;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
 
-;; Correctly add the local themes directory to the load-path
 (add-load-path! "themes")
-
-;; Some functionality uses this to identify you, e.g. GPG configuration, email
-;; clients, file templates and snippets. It is optional.
+(setq doom-theme 'doom-rose-pine)
+;; Set your name and email. Or don't. I don't care.
 (setq user-full-name "jesus"
       user-mail-address "theaminishere@gmail.com")
 
-;; It ensures all parts of Doom's UI use the same font consistently.
-(setq doom-font (font-spec :family "JetBrainsMono Nerd Font Mono" :size 12))
+;; This is the only directory that should matter to you.
+(setq org-directory "~/notes")
 
-;; Set your theme
-(setq doom-theme 'doom-rose-pine)
+;; that is actually installed on your system. Do not mess this up.
+(setq doom-font (font-spec :family "JetBrainsMono Nerd Font" :size 14))
 
-;; Configure line numbers
+(after! org
+  ;; This variable controls the bullets for headlines.
+  (setq org-superstar-headline-bullets-list '("•" "◆" "▲" "▶"))
+  ;; This makes list bullets prettier too.
+  (setq org-superstar-item-bullet-alist '((?+ . ?⋆) (?- . ?•))))
+;; Line numbers. A comfort for a Vim user.
+
+(use-package! org-modern :hook (org-mode . org-modern-mode))
+
+(use-package! doom-modeline
+  :config
+  (setq doom-modeline-icon t              ; Display icons
+        doom-modeline-major-mode-icon t   ; Display major mode icon
+        doom-modeline-buffer-file-name-style 'truncate-upto-project)) ; Clean up file path
+
 (setq display-line-numbers-type t)
-
-;; Set org-mode directories BEFORE the org module loads. This is correct.
-(setq org-directory "~/notes/")
-(setq org-roam-directory (file-truename "~/notes/roam/"))
-
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; CORRECTED AND SIMPLIFIED PACKAGE CONFIGURATION
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(use-package! org-roam
-  :after org
-  :custom
-  (org-roam-node-display-template (concat "${title:*} " (propertize "${tags:10}" 'face 'org-tag)))
-  (org-roam-capture-templates
-   '(("d" "default" plain "%?"
-      :if-new (file+head "${slug}.org"
-                         "#+title: ${title}\n")
-      :unnarrowed t)))
-  :config
-  (org-roam-db-autosync-mode)
-  (with-eval-after-load 'org
-    (add-hook 'org-mode-hook #'org-roam-mode)))
-
-
-(use-package! org-roam-ui
-  :after org-roam
-  :config
-  (setq org-roam-ui-sync-theme t
-        org-roam-ui-follow t
-        org-roam-ui-update-on-save t
-        org-roam-ui-open-on-start t))
-
-
-(use-package! vterm
-  :commands vterm)
